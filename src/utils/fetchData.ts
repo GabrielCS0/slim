f506e88b74f37326db17d07971c6a9978f0e06f8
Gabriel Cordeiro <gabrielcemp@gmail.com>
2022-10-27 16:18:48 -0300

    feat: add fetch data utility function

---
+++ src/utils/fetchData.ts
@@ -0,0 +1,15 @@
+export const exerciseDBRequestOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": String(process.env.REACT_APP_RAPID_API_KEY),
+      "X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
+    }
+};
+
+export const fetchData = async (url: RequestInfo, options: RequestInit) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data;
+};
+ 
