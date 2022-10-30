1a4073ffd2519d09ece08bf62a862ad633dba187
Gabriel Cordeiro <gabrielcemp@gmail.com>
2022-10-30 19:35:44 -0300

    feat: add exercise card component

---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Stack, Typography } from "@mui/material";
+
+import { ExerciseRequestResponseType } from "../pages/Home";
+
+type ExerciseCardData = {
+    exercise: ExerciseRequestResponseType;
+};
+
+const ExerciseCard = ({ exercise }: ExerciseCardData) => {
+  return (
+    <Link
+        to={`/exercises/${exercise.id}`}
+        className="exercise-card"
+    >
+        <img
+            src={exercise.gifUrl}
+            alt={exercise.name}
+            loading="lazy"
+        />
+        <Stack direction="row">
+            <Button
+                sx={{
+                    ml: "21px",
+                    color: "#FFFFFF",
+                    background: "#FFA9A9",
+                    fontSize: "14px",
+                    borderRadius: "20px",
+                    textTransform: "capitalize"
+                }}
+            >
+                {exercise.bodyPart}
+            </Button>
+            <Button
+                sx={{
+                    ml: "21px",
+                    color: "#FFFFFF",
+                    background: "#FCC757",
+                    fontSize: "14px",
+                    borderRadius: "20px",
+                    textTransform: "capitalize"
+                }}
+            >
+                {exercise.target}
+            </Button>
+        </Stack>
+
+        <Typography
+            ml="21px"
+            color="#000000"
+            fontWeight="bold"
+            mt="11px"
+            pb="10px"
+            textTransform="capitalize"
+            fontSize="22px"
+        >
+            {exercise.name}
+        </Typography>
+    </Link>
+  );
+};
+
+export default ExerciseCard;
