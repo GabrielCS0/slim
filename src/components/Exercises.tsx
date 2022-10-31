fe26a95cd04ae05615a64837ce9f76ae9e4189e7
Gabriel Cordeiro <gabrielcemp@gmail.com>
2022-10-31 12:29:18 -0300

    feat: add exercises component

--- src/components/Exercises.tsx
+++ src/components/Exercises.tsx
@@ -1,8 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Pagination from "@mui/material/Pagination";
+import { Box, Stack, Typography } from "@mui/material";
 
-const Exercises = () => {
+import { exerciseDBRequestOptions, fetchData } from "../utils/fetchData";
+import { ExerciseRequestResponseType } from "../pages/Home";
+import ExerciseCard from "./ExerciseCard";
+
+type ExercisesData = {
+  bodyPart: string;
+  exercises: ExerciseRequestResponseType[];
+  setExercises: React.Dispatch<React.SetStateAction<ExerciseRequestResponseType[]>>;
+}
+
+const Exercises = ({
+  bodyPart,
+  exercises,
+  setExercises
+}: ExercisesData) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  
+  const exercisesPerPage = 9;
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+  
+  const paginate = (event: React.ChangeEvent<unknown>, page: number) => {
+    setCurrentPage(page);
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === "all") {
+        exercisesData = await fetchData(
+          "https://exercisedb.p.rapidapi.com/exercises",
+          exerciseDBRequestOptions
+        );
+      } else {
+        exercisesData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseDBRequestOptions
+        );
+      };
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
+
   return (
-    <div>Exercises</div>
+    <Box
+      id="exercises"
+      mt="50px"
+      p="20px"
+      sx={{
+        mt: { lg: "110px" }
+      }}
+    >
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        flexWrap="wrap"
+        justifyContent="center"
+        sx={{
+          gap: { lg: "110px", xs: "50px" }
+        }}
+      >
+        {currentExercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+
+      <Stack mt="100px" alignItems="center">
+          {exercises.length > 9 && (
+            <Pagination
+              color="standard"
+              shape="rounded"
+              count={Math.ceil(exercises.length / exercisesPerPage)}
+              page={currentPage}
+              onChange={paginate}
+              size="large"
+              defaultPage={1}
+            />
+          )}
+      </Stack>
+    </Box>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,32 @@ import HeroBanner from "../components/HeroBanner";
 import SearchExercises from "../components/SearchExercises";
 import Exercises from "../components/Exercises";
 
+export type ExerciseRequestResponseType = {
+  bodyPart: string;
+  equipment: string;
+  gifUrl: string;
+  id: string;
+  name: string;
+  target: string;
+};
+
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState<ExerciseRequestResponseType[]>([]);
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+        setExercises={setExercises}
+      />
+      <Exercises
+        bodyPart={bodyPart}
+        exercises={exercises}
+        setExercises={setExercises}
+      />
     </Box>
   );
 };
