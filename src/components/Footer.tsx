687b57c3f38c8b37ccee54b0899273eeec586f73
Gabriel Cordeiro <gabrielcemp@gmail.com>
2022-11-04 11:50:40 -0300

    feat: add footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import Logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" bgcolor="#FFF3F4">
+      <Stack
+        gap="25px"
+        alignItems="center"
+        px="40px"
+        pt="24px"
+      >
+        <img src={Logo} alt={Logo} width="150px" height="35px" />
+        <Typography variant="h6" pb="28px" mt="8px" fontSize="14px">
+          © 2022
+        </Typography>
+      </Stack>
+    </Box>
   );
 };
 
